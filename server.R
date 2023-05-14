36409b4994e309f2a0371e803982b91a94d36c3c
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-05-13 20:17:32 -0700

    working on howler

--- server.R
+++ server.R
@@ -1,11 +1,12 @@
 library(howler)
 
 server <- function(input, output) {
+  NewSection = "https://cdn.pixabay.com/download/audio/2022/05/16/audio_db6591201e.mp3"
   #observeEvent(input$number, changeHowlerSeek("howler", input$number))
   #observeEvent(input$track, changeHowlerTrack("howler", input$track))
-  output$howleroutput <- renderHowler({
-    howler(
-      source("/audio.mp3"),
-      seek.connection(where=100))
-  })
+  #output$howleroutput <- renderHowler({
+    #howler(
+    #  source("/audio.mp3"),
+    #  seek.connection(where=100))
+  #})
 }
--- ui.R
+++ ui.R
@@ -1,7 +1,14 @@
 library(shiny)
 library(howler)
+library(tuneR)
 
 tracks <- c("https://cdn.pixabay.com/download/audio/2022/05/16/audio_db6591201e.mp3", "ShinyApp/audio1.mp3", "ShinyApp/audio2.mp3")
+
+#NewSection<-readWave("/Japanese.wav", from=10, to=30, units="seconds")
+
+audio_files_dir <- system.file("./audio", package = "howler")
+addResourcePath("ShinyApp", audio_files_dir)
+audio_files <- file.path("ShinyApp", list.files(audio_files_dir, ".wav$"))
 
 ui <- fluidPage(
   titlePanel("Infant Speech App"),
@@ -26,6 +33,19 @@ ui <- fluidPage(
       h1("audio panel"),
     )
   ),
-  h1("Howler Audio Player"),
-  howlerOutput("howleroutput")
+  howler::howlerModuleUI(
+    id = "sound2",
+    files = list(
+      "Test" = "NewSection"
+    )
+  ),
+  title = "howler.js Player",
+  howler(elementId = "sound", audio_files),
+  howlerCurrentTrack("sound"),
+  p(
+    howlerSeekTime("sound"),
+    "/",
+    howlerDurationTime("sound")
+  ),
+  howlerPlayPauseButton("sound")
 )
