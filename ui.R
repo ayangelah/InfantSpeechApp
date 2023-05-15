fa4170134a125b60f98fca1fb6f5651d1eed378c
ayang007 <86279159+ayang007@users.noreply.github.com>
2023-05-14 17:48:50 -0700

    more readability since merge to main

--- ui.R
+++ ui.R
@@ -4,14 +4,17 @@ library(tuneR)
 
 tracks <- c("https://cdn.pixabay.com/download/audio/2022/05/16/audio_db6591201e.mp3", "ShinyApp/audio1.mp3", "ShinyApp/audio2.mp3")
 
+#adds resource path
 addResourcePath("aud", "./audio")
 audio_files <- file.path("aud", list.files("./audio", ".wav$"))
 
+#makes temp file of clipped local file
 japanese_local_file <- file.path("./audio", "Japanese.wav")
 temp_file <- file.path("./audio", "temp.wav")
 writeWave(readWave(japanese_local_file, from=10, to=30, units="seconds"), filename=temp_file)
 seeked_audio_file <- file.path("aud", "temp.wav");
 
+#debug console local file
 print(paste0(audio_files))
 
 ui <- fluidPage(
@@ -37,12 +40,16 @@ ui <- fluidPage(
       h1("audio panel"),
     )
   ),
+  
+  #option 1 for howler UI
   howler::howlerModuleUI(
     id = "sound2",
     files = list(
       seeked_audio_file
     )
   ),
+  
+  #option 2 for howler UI
   title = "howler.js Player",
   howler(seeked_audio_file, elementId = "sound"),
   howlerCurrentTrack("sound"),
